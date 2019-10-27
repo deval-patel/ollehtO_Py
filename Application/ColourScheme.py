f4b83aad6632263f172f37af78cd6834db5563a3
Deval Patel <devalm.patel@mail.utoronto.ca>
2019-10-27 17:25:01 -0400

    Made GUI Application Run
    The Application.py file is what needs to be run in order to see
    the GUI.
    It makes the Tk(), and adds the significant frames into the root.
    The individual frames are constructed using some other classes which
    make the widgets and store it into their frames, these classes are,
    ImageFrame and ButtonFrame in the Widgets package.
    A Token class is created to act as a Button but provide us the
    (x,y) coordinate of where it is located so that it can be linked to the
    board in the future.
    Added a visual representation of the board, which is an 8 by 8 grid of
    buttons, this was made through the ImageFrame class.
    Added buttons for the Settings, Game Modes, and Quit options to be
    implemented in the future.
    Waiting on Othello model functionality to be completed so that the
    GUI (Application.py) can be linked to the model.

---
+++ Application/ColourScheme.py
@@ -0,0 +1,12 @@
+class ColourScheme:
+    """
+    The colour scheme for this application
+    """
+    BACKGROUND: str
+    TEXT_COLOUR: str
+    BUTTON_COLOUR: str
+
+    def __init__(self):
+        self.BACKGROUND = "#558C3B"
+        self.TEXT_COLOUR = "white"
+        self.BUTTON_COLOUR = "#204037"
---
+++ Application/Widgets/ButtonFrame.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from othello.Application.ColourScheme import ColourScheme
+class ButtonFrame(Frame):
+    """
+    Todo: Explain
+    """
+    colour: ColourScheme
+    game_mode: Button
+    settings: Button
+    quit: Button
+    space_holder_1: Label
+    space_holder_2: Label
+
+    def __init__(self, master, colour_scheme, **kw):
+        super().__init__(master=master, **kw)
+        self.colour = colour_scheme
+        self.config(bd=10, bg=self.colour.BACKGROUND, width=520)
+        # Button Frame
+        self.game_button = Button(self, text="Game Modes", font=("Times New Roman", 16), foreground=self.colour.TEXT_COLOUR,
+                                  width=18, padx=0, bg=self.colour.BUTTON_COLOUR)
+        self.game_button.grid(row=0, column=0)
+        space_holder_1 = Label(self, width=15, bg=self.colour.BACKGROUND)
+        space_holder_1.grid(row=0, column=1)
+        self.settings = Button(self, text="Settings", font=("Times New Roman", 16), foreground=self.colour.TEXT_COLOUR,
+                               width=18, padx=0, bg=self.colour.BUTTON_COLOUR)
+        self.settings.grid(row=0, column=2)
+        space_holder_2 = Label(self, width=15, bg=self.colour.BACKGROUND)
+        space_holder_2.grid(row=0, column=3)
+        self.quit = Button(self, text="Exit", font=("Times New Roman", 16), foreground=self.colour.TEXT_COLOUR,
+                           width=18, padx=0, bg=self.colour.BUTTON_COLOUR)
+        self.quit.grid(row=0, column=4)
+
+
