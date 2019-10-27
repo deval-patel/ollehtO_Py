4476f192320f9cc9c17dffc541effdb9f91e2777
Brendan Tiu <brendan.tiu@mail.utoronto.ca>
2019-10-27 17:50:01 -0400

    Create play method for controller superclass
    The play method is responsible for continuously
    getting move from a player and making a move on
    the board until the game ends. For testing purposes,
    there will be reports made to the console on various
    game information every time a move is made and when
    the game is over.

--- othello/Controllers/controller.py
+++ othello/Controllers/controller.py
@@ -1,6 +1,7 @@
 from othello.OthelloGame.othello import Othello
 from othello.player.player import Player
 from othello.player.move import Move
+from othello.OthelloGame.board import Board
 
 
 class Controller:
@@ -29,3 +30,48 @@ class Controller:
         """
 
         self.othello = Othello()
+
+    def play(self):
+        """
+        If the game is not over, print the current board state
+        and keep getting moves from whoever's turn it is,
+        reporting the moves made to the console. When the game
+        is over, report who the winner is
+
+        """
+        while not self.othello.check_game_over():
+            whose_turn = self.othello.get_current_player()
+            self.report()
+
+            if whose_turn == Board.player1:
+                move = self.player_one.getMove()
+            if whose_turn == Board.player2:
+                move = self.player_two.getMove()
+
+            self.report_move(whose_turn, move)
+            self.othello.move(move.getRow(), move.getCol())
+
+        self.report_final()
+
+    def report(self):
+        """
+        Prints the current board state to the console
+        """
+
+        raise NotImplementedError
+
+    def report_move(self, whose_turn: str, move: Move):
+        """
+        Print the player who made a move and the move they
+        made on the specific coordinates on the board to the
+        console.
+        """
+
+        raise NotImplementedError
+
+    def report_final(self):
+        """
+        Print the final board state of the game and who won the game
+        """
+
+        raise NotImplementedError
