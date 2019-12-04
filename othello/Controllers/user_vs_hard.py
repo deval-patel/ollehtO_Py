b845d7e40dcd9fe7c45bf12864b68d89223f8006
Brendan Tiu <brendan.tiu@mail.utoronto.ca>
2019-12-04 17:31:48 -0500

    Write documentation for user_vs_hard

--- othello/Controllers/user_vs_hard.py
+++ othello/Controllers/user_vs_hard.py
@@ -5,14 +5,26 @@ from othello.OthelloGame.board import Board
 
 
 class UserVSHard(Controller):
+    """
+    This class is responsible for instantiating the human and hard AI
+    players and running the controller through the main class
+    """
 
     def __init__(self):
+        """
+        Initialize a human player and a hard AI
+        """
+
         super().__init__()
         self.player_one = PlayerHuman(self.othello, Board.player1)
         self.player_two = PlayerHard(self.othello, Board.player2)
 
 
 def main():
+    """
+    Run this controller
+    """
+
     othello_controller = UserVSHard()
     othello_controller.play()
 
