1559e3cf5220ce93efc9dd8fc73912b2299066d3
Brendan Tiu <brendan.tiu@mail.utoronto.ca>
2019-12-04 17:32:07 -0500

    Write documentation for user_vs_user

--- othello/Controllers/user_vs_user.py
+++ othello/Controllers/user_vs_user.py
@@ -4,14 +4,26 @@ from othello.OthelloGame.board import Board
 
 
 class UserVSUser(Controller):
+    """
+    This class is responsible for instantiating two human players
+    players and running the controller through the main class
+    """
 
     def __init__(self):
+        """
+        Initialize a human player and a medium AI
+        """
+
         super().__init__()
         self.player_one = PlayerHuman(self.othello, Board.player1)
         self.player_two = PlayerHuman(self.othello, Board.player2)
 
 
 def main():
+    """
+    Run this controller
+    """
+
     othello_controller = UserVSUser()
     othello_controller.play()
 
