018343cee01b273b466992ffb4b5a2f7739cb8ad
Brendan Tiu <brendan.tiu@mail.utoronto.ca>
2019-12-04 17:31:58 -0500

    Write documentation for user_vs_medium

--- othello/Controllers/user_vs_medium.py
+++ othello/Controllers/user_vs_medium.py
@@ -5,14 +5,26 @@ from othello.OthelloGame.board import Board
 
 
 class UserVSMedium(Controller):
+    """
+    This class is responsible for instantiating the human and medium AI
+    players and running the controller through the main class
+    """
 
     def __init__(self):
+        """
+        Initialize a human player and a medium AI
+        """
+
         super().__init__()
         self.player_one = PlayerHuman(self.othello, Board.player1)
         self.player_two = PlayerMedium(self.othello, Board.player2)
 
 
 def main():
+    """
+    Run this controller
+    """
+
     othello_controller = UserVSMedium()
     othello_controller.play()
 
