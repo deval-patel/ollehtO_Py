6ce35b06da5533601db8a14b363ad7908bbfdbe9
Brendan Tiu <brendan.tiu@mail.utoronto.ca>
2019-12-04 17:31:37 -0500

    Write documentation for user_vs_easy

--- othello/Controllers/user_vs_easy.py
+++ othello/Controllers/user_vs_easy.py
@@ -5,14 +5,26 @@ from othello.OthelloGame.board import Board
 
 
 class UserVSEasy(Controller):
+    """
+    This class is responsible for instantiating the human and easy AI
+    players and running the controller through the main class
+    """
 
     def __init__(self):
+        """
+        Initialize a human player and an easy AI
+        """
+
         super().__init__()
         self.player_one = PlayerHuman(self.othello, Board.player1)
         self.player_two = PlayerEasy(self.othello, Board.player2)
 
 
 def main():
+    """
+    Run this controller
+    """
+
     othello_controller = UserVSEasy()
     othello_controller.play()
 
